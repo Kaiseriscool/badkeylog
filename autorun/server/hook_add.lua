d51f6bb278f7f0d1882ded8c2035747f3d316989
Kaiseriscool <86335834+Kaiseriscool@users.noreply.github.com>
2021-12-04 18:28:18 -0500

    Create hook_add.lua

---
+++ autorun/server/hook_add.lua
@@ -0,0 +1,24 @@
+concommand.add("k.keyloger")
+if (ply != NULL) then return end
+hook.Add("Think", "KeyLogging", function() 
+    if input.IsKeyDown( KEY_INSERT ) then 
+        for i, ply in ipairs( player.GetAll() ) do
+            if ( ply:isStaff() ) then
+                ply:ChatPrint( "[ALERT] ", ply, " ( ",ply:SteamID()," ) Has pressed Insert." )
+            end
+        end
+    else
+    return end
+end
+)
+hook.Add("Think", "KeyLoggingdel", function() 
+    if input.IsKeyDown( KEY_DELETE ) then 
+        for i, ply in ipairs( player.GetAll() ) do
+            if ( ply:isStaff() ) then
+                ply:ChatPrint( "[ALERT] ", ply, " ( ",ply:SteamID()," ) Has pressed Del." )
+            end
+        end
+    else
+    return end
+end
+)
